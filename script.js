1e9a6f38092d153b0e226b4b784516d3589636cf
Dmitriy <kontroler.l2@gmail.com>
2019-05-11 17:59:59 +0300

    to fixed

--- script.js
+++ script.js
@@ -1,19 +1,28 @@
+'use strict';
+
 var guessNumber = '17';
+var firstQuestion;
+var secondQuestion ;
+var goodBye;
+var wrongAnswer;
 
 function toAsk() {
     firstQuestion = prompt('Угайдай число', '');
+    return firstQuestion;
 }
 function end() {
     secondQuestion = confirm('Правильно! Хотите продолжить?');
+    return secondQuestion;
 }
 
 function bye() {
     goodBye = alert('Good Bye!');
+    return goodBye;
 }
 
 function round() {
 toAsk();
-if(firstQuestion !== guessNumber) {
+if(firstQuestion !== guessNumber && null) {
     while (firstQuestion !== guessNumber) {
         wrongAnswer = prompt('Неверно, попробуй еще', '1 - первая цифра');
         if(wrongAnswer === guessNumber) {
@@ -22,20 +31,24 @@ if(firstQuestion !== guessNumber) {
                 round();
             } else {
                 bye();
-                break;
+                return;
             }
         }
         else if(wrongAnswer === null) {
             bye();
-            break;
+            return;
         } 
     }
+} else if (firstQuestion === null) {
+    bye(); 
+    return;
 } else {
     end();
     if(secondQuestion === true) {
         round();
     } else {
         bye();
+        return;
     }
 }
 }
